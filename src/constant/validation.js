79ac34b248cb7a1000e39dfa9570eb7d1c6a44ef
mohitkumarcard91 <mohit.kumar@card91..io>
2025-12-31 14:04:16 +0530

    add the vercel.json

--- src/constant/validation.js
+++ src/constant/validation.js
@@ -3,7 +3,11 @@ import * as yup from 'yup';
 const userSchema = yup.object({
   name: yup.string().min(3, "Name must be at least 3 characters").max(30).required("Name is required"),
   email: yup.string().email("Invalid email format").required("Email is required"),
-  password: yup.string().min(6, "Password must be at least 6 characters").max(20).required("Password is required"),
+  password: yup.string .min(6, 'Password must be at least 8 characters')
+    .matches(/[A-Z]/, 'Password must contain at least one uppercase letter')
+    .matches(/[0-9]/, 'Password must contain at least one number')
+    .max(20, 'Password must be at most 20 characters')
+    .required("Password is required"),
 });
 
 
