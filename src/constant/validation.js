0f2aa95992952bad8253da525580b97ff77c70b1
mohitkumarcard91 <mohit.kumar@card91..io>
2025-12-31 14:06:52 +0530

    update the validation

--- src/constant/validation.js
+++ src/constant/validation.js
@@ -3,7 +3,7 @@ import * as yup from 'yup';
 const userSchema = yup.object({
   name: yup.string().min(3, "Name must be at least 3 characters").max(30).required("Name is required"),
   email: yup.string().email("Invalid email format").required("Email is required"),
-  password: yup.string .min(6, 'Password must be at least 8 characters')
+  password: yup.string() .min(6, 'Password must be at least 8 characters')
     .matches(/[A-Z]/, 'Password must contain at least one uppercase letter')
     .matches(/[0-9]/, 'Password must contain at least one number')
     .max(20, 'Password must be at most 20 characters')
